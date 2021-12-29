b3a86e8d2c0f4c5b0663cf6a824fa8da71b05683
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-12-28 22:16:34 -0300

    Complete all users routes handlers
    Create scaffolding for users methods
    
    Create helper for hashing passwords
    
    Create users creation handler
    
    Create users handler post method
    
    Create users handler get method
    
    Create users handler put  method
    
    Create users handler delete method

--- config.js
+++ config.js
@@ -3,19 +3,22 @@ const environments = {};
 environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
-  envName: 'staging'
+  envName: 'staging',
+  hashingSecret: 'secret',
 };
 
 environments.development = {
   httpPort: 3300,
   httpsPort: 3301,
-  envName: 'development'
+  envName: 'development',
+  hashingSecret: 'secret',
 };
 
 environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
-  envName: 'production'
+  envName: 'production',
+  hashingSecret: 'secret',
 };
 
 const currentEnv = typeof(process.env.NODE_ENV) === "string" ? process.env.NODE_ENV.toLowerCase() : '';
