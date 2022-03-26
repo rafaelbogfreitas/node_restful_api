427e61b443d74d3e137e2756d029235bb6e91ef7
Rafael Freitas <rafaelbogfreitas@gmail.com>
2022-03-25 23:36:40 -0300

    Create base test structure and initial tests (#27)
    Finish end to end tests base
    
    Co-authored-by: Rafael Freitas <rafaelborges1985@hotmail.com>

--- config.js
+++ config.js
@@ -57,6 +57,25 @@ environments.production = {
   }
 };
 
+environments.testing = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'testing',
+  hashingSecret: 'secret',
+  maxChecks: 5,
+  twilio: {
+    fromPhone: process.env.FROM_PHONE,
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN,
+  },
+  templateGlobals: {
+    appName: "uptimeChecker",
+    companyName: "TestCompany",
+    yearCreated: "2022",
+    baseUrl: "http://localhost:3000",
+  }
+};
+
 const currentEnv = typeof(process.env.NODE_ENV) === "string" ? process.env.NODE_ENV.toLowerCase() : '';
 
 const envToExport = typeof(environments[currentEnv]) === "object" ? environments[currentEnv] : environments.staging;
