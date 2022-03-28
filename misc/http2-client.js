928f088e644d97ad202d8eb9872ef7227547bf58
Rafael Freitas <rafaelbogfreitas@gmail.com>
2022-03-28 07:55:58 -0300

    Create example http2 client and server (#31)
    Co-authored-by: Rafael Freitas <rafaelborges1985@hotmail.com>

---
+++ misc/http2-client.js
@@ -0,0 +1,18 @@
+const http2 = require("http2");
+
+const client = http2.connect("http://localhost:6000");
+
+const req = client.request({
+  ":path": "/",
+});
+
+let str = '';
+req.on("data", (chunk) => {
+  str += chunk;
+});
+
+req.on('end', () => {
+  console.log(str);
+});
+
+req.end();
